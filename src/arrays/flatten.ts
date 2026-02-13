38871449dfadf28167fbce3495956a4851dcab81
Phil Wilt <phillip.wilt@gmail.com>
2026-02-12 19:01:16 -0800

    Cleanup

--- src/arrays/flatten.ts
+++ src/arrays/flatten.ts
@@ -3,12 +3,14 @@
  * Exercise stub for flattening nested arrays.
  */
 
-export const flatten = <T>(arr: any[]): T[] => {
+type NestedArray<T> = (T | NestedArray<T>)[];
+
+export const flatten = <T>(arr: NestedArray<T>): T[] => {
   const result: T[] = [];
 
   for (const item of arr) {
     if (Array.isArray(item)) {
-      result.push(...flatten(item));
+      result.push(...flatten<T>(item));
     } else {
       result.push(item);
     }
