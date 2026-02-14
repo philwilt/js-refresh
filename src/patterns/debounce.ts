c49deb71bc2579d44c3472ec3c6f2df509ba3ae8
Phil Wilt <phillip.wilt@gmail.com>
2026-02-14 10:12:49 -0800

    Add another

--- src/patterns/debounce.ts
+++ src/patterns/debounce.ts
@@ -2,3 +2,22 @@
  * src/patterns/debounce.ts
  * Exercise stub for implementing debounce behavior.
  */
+export const debounce = <Args extends unknown[]>(
+  fn: (...args: Args) => unknown,
+  delay: number,
+): ((...vals: Args) => void) => {
+  let delaying = false;
+  let handle: ReturnType<typeof setTimeout>;
+
+  return (...vals: Args): void => {
+    if (delaying) {
+      clearTimeout(handle);
+    }
+
+    handle = setTimeout(() => {
+      fn(...vals);
+    }, delay);
+
+    delaying = true;
+  };
+};
