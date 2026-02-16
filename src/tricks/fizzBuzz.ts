64de72287a9f2ec54f2ebdc9993472bc9a078666
Phil Wilt <phillip.wilt@gmail.com>
2026-02-16 10:49:19 -0800

    Add another implimentation of fizzbuzz

--- src/tricks/fizzBuzz.ts
+++ src/tricks/fizzBuzz.ts
@@ -1,4 +1,4 @@
-export const fizzBuzz = (nArr: number[]): void => {
+export const fizzBuzzWithLogging = (nArr: number[]): void => {
   nArr.forEach((n) => {
     if (n % 3 === 0 && n % 5 === 0) {
       console.log("FizzBuzz");
@@ -9,5 +9,14 @@ export const fizzBuzz = (nArr: number[]): void => {
     } else {
       console.log(n.toString());
     }
+  });
+};
+
+export const fizzBuzz = (nArr: number[]): string[] => {
+  return nArr.map((n) => {
+    if (n % 15 === 0) return "FizzBuzz";
+    if (n % 3 === 0) return "Fizz";
+    if (n % 5 === 0) return "Buzz";
+    return n.toString();
   });
 };
--- tests/tricks/fizzBuzz.test.ts
+++ tests/tricks/fizzBuzz.test.ts
@@ -1,6 +1,7 @@
 import { describe, expect, it, vi, beforeEach, afterEach } from "vitest";
-import { fizzBuzz } from "../../src/tricks/fizzBuzz";
-describe("fizzBuzz", () => {
+import { fizzBuzz, fizzBuzzWithLogging } from "../../src/tricks/fizzBuzz";
+
+describe("fizzBuzz with console.log spying", () => {
   let spy: ReturnType<typeof vi.spyOn>;
 
   beforeEach(() => {
@@ -12,31 +13,31 @@ describe("fizzBuzz", () => {
   });
 
   it("prints Fizz if a number is divisible by 3", () => {
-    fizzBuzz([3]);
+    fizzBuzzWithLogging([3]);
 
     expect(spy).toHaveBeenCalledWith("Fizz");
   });
 
   it("prints Buzz if a number is divisible by 5", () => {
-    fizzBuzz([5]);
+    fizzBuzzWithLogging([5]);
 
     expect(spy).toHaveBeenCalledWith("Buzz");
   });
 
   it("prints FizzBuzz if a number is divisible by 3 & 5", () => {
-    fizzBuzz([15]);
+    fizzBuzzWithLogging([15]);
 
     expect(spy).toHaveBeenCalledWith("FizzBuzz");
   });
 
   it("prints just the number of it is not divisible by 3 or 5", () => {
-    fizzBuzz([2]);
+    fizzBuzzWithLogging([2]);
 
     expect(spy).toHaveBeenCalledWith("2");
   });
 
   it("prints the correct numbers and strings for an array of numbers to 5 and 15", () => {
-    fizzBuzz([1, 2, 3, 4, 5, 15]);
+    fizzBuzzWithLogging([1, 2, 3, 4, 5, 15]);
 
     expect(spy.mock.calls).toEqual([
       ["1"],
@@ -45,6 +46,32 @@ describe("fizzBuzz", () => {
       ["4"],
       ["Buzz"],
       ["FizzBuzz"],
+    ]);
+  });
+});
+
+describe("fizzBuzz using return value", () => {
+  it("returns Fizz with 3", () => {
+    expect(fizzBuzz([3])).toEqual(["Fizz"]);
+  });
+
+  it("returns Buzz with 5", () => {
+    expect(fizzBuzz([5])).toEqual(["Buzz"]);
+  });
+
+  it("returns FizzBuzz with 15", () => {
+    expect(fizzBuzz([15])).toEqual(["FizzBuzz"]);
+  });
+
+  it("returns correct numbers and strings for an array", () => {
+    expect(fizzBuzz([1, 2, 3, 4, 5, 15, 16])).toEqual([
+      "1",
+      "2",
+      "Fizz",
+      "4",
+      "Buzz",
+      "FizzBuzz",
+      "16",
     ]);
   });
 });
